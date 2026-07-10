fe2346ffab8d6bfd81722895fa41a2981ed1204e
David Vaughan <david@tynobryn.co.uk>
2026-07-10 20:20:16 +0000

    devalue c2 to make c4 happen. careful balance required here to not think opponent having a pawn on c7 is a massive positional weakness

--- src/eval.hpp
+++ src/eval.hpp
@@ -29,7 +29,7 @@
     // LERF Mapped Pawn PST (Index 0 is a1, Index 63 is h8)
     constexpr int PST_PAWN[64] = {
           0,   0,   0,   0,   0,   0,   0,   0,  // Rank 1 (Indices 0-7)
-          5,  10,  -20, -5, -5,  10,  10,   5,  // Rank 2 (Indices 8-15) - discourage e2/d2 slightly to open bishop/queen lanes
+          5,  10,  -60, -5, -5,  10,  10,   5,  // Rank 2 (Indices 8-15) - discourage c2 as 1.c4 best opening
           5,  -5, -10,   0,   0, -10,  -5,   5,  // Rank 3 (Indices 16-23)
           0,   0,   30,  15,  15,   0,   0,   0,  // Rank 4 (Indices 24-31) - Encourage center pushes
           5,   5,  10,  25,  25,  10,   5,   5,  // Rank 5 (Indices 32-39)
